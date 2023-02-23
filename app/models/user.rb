6cdbabd01b9b7bf3cf65c4362c8f086934ca1eb9
Corey <coreyzhee@gmail.com>
2023-02-23 16:33:29 -0500

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :favorites
+    has_many :albums, through: :favorites
+    has_many :carts
+    has_many :line_items, through: :cart
 end
--- db/migrate/20230223071456_create_favorites.rb
+++ db/migrate/20230223071456_create_favorites.rb
@@ -1,7 +1,7 @@
 class CreateFavorites < ActiveRecord::Migration[7.0]
   def change
     create_table :favorites do |t|
-      t.integer :favorites_id
+      t.integer :favorite_id
       t.integer :album_id
       t.integer :user_id
 
